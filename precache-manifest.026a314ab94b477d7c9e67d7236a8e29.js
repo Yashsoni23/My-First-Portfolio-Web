c974ff175aa43122b132ce820e4c7ddef98cac1e
Yash simejiya <yashsoni48678@gmail.com>
2022-08-29 01:17:02 +0530

    Updates

---
+++ precache-manifest.026a314ab94b477d7c9e67d7236a8e29.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "66fc532fca68b05a2736",
+    "url": "/YashWebWithReact/yash-soni/static/css/main.c8770280.chunk.css"
+  },
+  {
+    "revision": "66fc532fca68b05a2736",
+    "url": "/YashWebWithReact/yash-soni/static/js/main.66fc532f.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/YashWebWithReact/yash-soni/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "deb2b476bb9921bc8f58",
+    "url": "/YashWebWithReact/yash-soni/static/js/2.deb2b476.chunk.js"
+  },
+  {
+    "revision": "643d371261e035635b52",
+    "url": "/YashWebWithReact/yash-soni/static/js/runtime~main.643d3712.js"
+  },
+  {
+    "revision": "d23e2b3d9f35bc32b09580036f71cc24",
+    "url": "/YashWebWithReact/yash-soni/static/media/OIP.d23e2b3d.jfif"
+  },
+  {
+    "revision": "405c0e268ed4da367d0a49eb72270e80",
+    "url": "/YashWebWithReact/yash-soni/static/media/Ecommerce.405c0e26.jpg"
+  },
+  {
+    "revision": "f1cacc647fb8b91d7e701342de0e5c7c",
+    "url": "/YashWebWithReact/yash-soni/static/media/685-6854994_react-logo-no-background-hd-png-download.f1cacc64.png"
+  },
+  {
+    "revision": "e19f25fec5488c20f3ed10aba52705e4",
+    "url": "/YashWebWithReact/yash-soni/static/media/YASH.e19f25fe.jpg"
+  },
+  {
+    "revision": "c1c6389ae0e868b312bf20302ab1885f",
+    "url": "/YashWebWithReact/yash-soni/static/media/bank.c1c6389a.jpg"
+  },
+  {
+    "revision": "4b057e7eb7d7c0f0da575514c9760dc3",
+    "url": "/YashWebWithReact/yash-soni/static/media/Portfolio.4b057e7e.jpg"
+  },
+  {
+    "revision": "8d6c4c84726179a78ce0a6ec56159d01",
+    "url": "/YashWebWithReact/yash-soni/static/media/Yash_Poster2.8d6c4c84.JPG"
+  },
+  {
+    "revision": "274486ba32dbe68b2cadb330ad58316f",
+    "url": "/YashWebWithReact/yash-soni/static/media/yash254.274486ba.jpg"
+  },
+  {
+    "revision": "314c5b007b790a5e2d58fe816bdecfcb",
+    "url": "/YashWebWithReact/yash-soni/index.html"
+  }
+];
